d505bec8c7e99ac6bb1d9a2b4a8399381d6b526c
bcc102142 <bcc102142@aliyun.com>
2019-04-09 08:46:01 +0800

    做详情页\

--- src/pages/home/soundDetail/SoundDetail.jsx
+++ src/pages/home/soundDetail/SoundDetail.jsx
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
 
-import {SoundDetailStyled,BorderedSoundActionContainer} from './SoundDetailStyled'
+import {SoundDetailStyled,BorderedSoundActionContainer,BorderedSoundInfo,BorderedSoundDrama,BorderedSoundCV} from './SoundDetailStyled'
 
+import http from 'utils/fetch'
 import pixelate from 'assets/js/close-pixelate'
 
 class SoundDetail extends Component {
+    constructor(props){
+        super(props)
+        this.state ={
+            value : '更多',
+            data1 : []
+        }
+        this.clickHandler = this.clickHandler.bind(this)
+        this.fetchHandler()
+    }
+   async fetchHandler(){
+        console.log()
+        let url = '/sound/getsound?soundid='+this.props.match.params.id
+        let data = await http.get(url)
+        this.setState({
+            data1 : data.info
+        })
+    }
     render() {
+        let sound = this.state.data1.sound ? this.state.data1.sound : {}
         return (
-            <SoundDetailStyled>
+            <SoundDetailStyled value={this.state.value}>
                 <div id="soundPlayer" > 
-                    <div className="imgBox"><img id="portrait-image" style={{width:"2.2rem",height:"2.2rem"}} src="//static.missevan.com/coversmini/201901/09/9bd6b216f4ee808757785359a2a11216092822.jpeg" alt=""/></div>
+                    <div className="imgBox"><img id="portrait-image" style={{width:"2.2rem",height:"2.2rem"}} src={sound.front_cover} alt=""/></div>
                     <div className="soundController">
                         <div className="btnPlay"></div>
                     </div>
@@ -44,26 +63,156 @@ class SoundDetail extends Component {
                 </div>
                 <div className="tabContent">
                     <div>
-                        <div className="soundInfo">
+                        <BorderedSoundInfo value={this.state.value}>
                             <p>【兴津和幸×齐藤壮马】テオ-THEO-第 1 话</p>
                             <ul>
                                 <li>5.6万</li>
                                 <li>603</li>
                                 <li>音频id:1144352</li>
                             </ul>
-                            <p>简介：</p>
-                            <div>
-                                <p>我真的很勤奋相信我（其实是为了下个星期不更新找借口）</p>
+                            <div className="infoBox">
+                                <div className="intro">
+                                    <p>简介：</p>
+                                    <p>我真的很勤奋相信我（其实是为了下个星期不更新找借口）</p>
+                                    <p>这个抓当时看到画风就是心动的感觉了（加上是我推的要支持一下）</p>
+                                    <p>收到的时候立马就听了呜呜呜真的是慢悠悠的治愈真好。</p>
+                                    <p>——————喜欢请支持正版——————————</p>
+                                    
+                                    <p>「僕が恋をしたのは神様でした」<br/>この国には、魃(バツ)と呼ばれる神様がいる。<br/>少年テオが仕えるのは、夢のように美しい魃・レイ。<br/>血筋によって決められただけの主従関係だったが、次第にそれは恋心へと変わっていく。<br/>そばにいたいと思う気持ちは日増しに強くなり、熱い身体に触れることも、心地よく感じるようになった。<br/>しかし、魃は人間の数倍の速さで歳を取り、レイもまた、必ずテオより先に寿命を迎えるのだという――。<br/>これは移ろいゆく四季のなかでふたりの永遠を紡ぐ御伽噺。<br/>あおのなちが描く、あたたかくて切ない、神様×健気な少年の恋路をドラマCD化！<br/>巡る四季の情景と共に深まる愛をお楽しみください。<br/></p>
+
+                                    <p>【出演者】テオ（受）：斉藤壮馬、</p>
+                                    <p>レイ（攻）：興津和幸</p>
+                                </div>
+                               <span onClick={this.clickHandler}>{this.state.value}</span>
                             </div>
+                        </BorderedSoundInfo>
+                        <BorderedSoundDrama href="/drama/20542">
+                            <div><img style={{height:".6rem",width:".6rem"}} src="//static.missevan.com/dramacoversmini/201903/01/c6606369b23be8675007dec622c40526163514.png" alt=""/></div>
+                            <div className="soundDramaRight">
+                                <div className="soundDramaRightText">
+                                    <p>缇欧-THEO-</p>
+                                    <span>日文有声漫画 类型：纯爱</span>
+                                    <span>更新至 FT</span>
+                                </div>
+                                <div className="soundDramaRightBtn">追剧</div>
+                            </div>
+                        </BorderedSoundDrama>
+                        <BorderedSoundCV>
+                            <h4>参演CV</h4>
+                            <div className="cv-in-sound-list">
+                                <div className="cv-in-sound">
+                                    <div><img style={{width:".46rem",height:".46rem",borderRadius:"50%"}} src="//static.missevan.com/seiys/201808/31/e976decfed7c7728e5d3397ebd913608143757.jpg" alt=""/></div>
+                                    <p>齐藤壮马</p>
+                                    <span>饰：テオ</span>
+                                    <span>81 Produce</span>
+                                </div>
+                                <div className="cv-in-sound">
+                                    <div><img style={{width:".46rem",height:".46rem",borderRadius:"50%"}} src="//static.missevan.com/seiys/201808/31/e976decfed7c7728e5d3397ebd913608143757.jpg" alt=""/></div>
+                                    <p>齐藤壮马</p>
+                                    <span>饰：テオ</span>
+                                    <span>81 Produce</span>
+                                </div>
+                            </div>
+                        </BorderedSoundCV>
+
+                        <BorderedSoundDrama href="/drama/20542">
+                            <div><img style={{height:".6rem",width:".6rem",borderRadius:"50%"}} src="//static.missevan.com/dramacoversmini/201903/01/c6606369b23be8675007dec622c40526163514.png" alt=""/></div>
+                            <div className="soundDramaRight">
+                                <div className="soundDramaRightText">
+                                    <p>缇欧-THEO-</p>
+                                    <span>发布于：2019/01/09 17:28</span>
+                                </div>
+                                <div className="soundDramaRightBtn">关注</div>
+                            </div>
+                        </BorderedSoundDrama>
+
+                        <div className="soundRelative">
+                            <div className="channel-title">
+                                <div className="channel-title-head">
+                                    音频相关
+                                </div>
+                            </div>
+                            <div className="sound-block">
+                                <p>剧集推荐</p>
+                                <div className="sound-block-recommend">
+                                    <a href="#">
+                                        <div><img style={{width:"1.05rem",height:"1.36rem"}} src="//static.missevan.com/dramacoversmini/201807/19/8167478c911ea504a0613b0ed6fce0b9162059.png" alt=""/></div>
+                                    <span>K 诸王归来 Prelude 青&白银/赤&绿</span>
+                                    
+                                    </a>
+                                    <a href="#">
+                                        <div><img style={{width:"1.05rem",height:"1.36rem"}} src="//static.missevan.com/dramacoversmini/201807/19/8167478c911ea504a0613b0ed6fce0b9162059.png" alt=""/></div>
+                                    <span>K 诸王归来 Prelude 青&白银/赤&绿</span>
+                                    
+                                    </a>
+                                    <a href="#">
+                                        <div><img style={{width:"1.05rem",height:"1.36rem"}} src="//static.missevan.com/dramacoversmini/201807/19/8167478c911ea504a0613b0ed6fce0b9162059.png" alt=""/></div>
+                                    <span>K 诸王归来 Prelude 青&白银/赤&绿</span>
+                                    
+                                    </a>
+                                    <a href="#">
+                                        <div><img style={{width:"1.05rem",height:"1.36rem"}} src="//static.missevan.com/dramacoversmini/201807/19/8167478c911ea504a0613b0ed6fce0b9162059.png" alt=""/></div>
+                                    <span>K 诸王归来 Prelude 青&白银/赤&绿</span>
+                                    
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div className="sound-block">
+                                <p>相似音频</p>
+                                <div className="sound-block-like">
+                                    <a href="#">
+                                        <img  src='//static.missevan.com/dramacoversmini/201807/19/8167478c911ea504a0613b0ed6fce0b9162059.png' alt=""/>
+                                        <div className="channel-body-title">aaaaa</div>
+                                        <div className="channel-body-bottom">
+                                            <span className="channel-body-palytimes">3512</span>
+                                            <span className="channel-body-comments">5214</span>
+                                        </div>
+                                    </a>
+                                    <a href="#">
+                                        <img  src='//static.missevan.com/dramacoversmini/201807/19/8167478c911ea504a0613b0ed6fce0b9162059.png' alt=""/>
+                                        <div className="channel-body-title">aaaaa</div>
+                                        <div className="channel-body-bottom">
+                                            <span className="channel-body-palytimes">3512</span>
+                                            <span className="channel-body-comments">5214</span>
+                                        </div>
+                                    </a>
+                                    <a href="#">
+                                        <img  src='//static.missevan.com/dramacoversmini/201807/19/8167478c911ea504a0613b0ed6fce0b9162059.png' alt=""/>
+                                        <div className="channel-body-title">aaaaa</div>
+                                        <div className="channel-body-bottom">
+                                            <span className="channel-body-palytimes">3512</span>
+                                            <span className="channel-body-comments">5214</span>
+                                        </div>
+                                    </a>
+                                    <a href="#">
+                                        <img  src='//static.missevan.com/dramacoversmini/201807/19/8167478c911ea504a0613b0ed6fce0b9162059.png' alt=""/>
+                                        <div className="channel-body-title">aaaaa</div>
+                                        <div className="channel-body-bottom">
+                                            <span className="channel-body-palytimes">3512</span>
+                                            <span className="channel-body-comments">5214</span>
+                                        </div>
+                                    </a>
+                              
+                                </div>
+                            </div>
+
                         </div>
                     </div>
-                
                 </div>
-                        
-                
-
             </SoundDetailStyled>
         );
+    }
+    clickHandler(){
+        if(this.state.value === '更多'){
+            this.setState({
+                value : '收起'
+            })
+        }else{
+            this.setState({
+                value : '更多'
+            })
+        }
     }
    
 
--- src/pages/home/soundDetail/SoundDetailStyled.js
+++ src/pages/home/soundDetail/SoundDetailStyled.js
@@ -156,19 +156,217 @@ const SoundDetailStyled = Styled.div`
     }
     .tabContent{
             padding : .12rem
-            .soundInfo{
-                ul{
-                    display:flex;
+        
 
                 }
             }
         }
    
 }
-
+.soundRelative{
+           
+    .channel-title{
+        height: .24rem;
+        line-height: .4rem;
+        display: flex;
+        position: relative;
+        justify-content: space-between;
+        padding: 0 .14rem 0 .14rem;
+        margin: .1rem 0;
+        align-items:center
+        .channel-title-head{
+        font-size: 16px;
+        text-align:center;
+        i{
+            display:inline-block;
+            background-size: 86px 86px;
+            position: relative;
+            top: .04rem;
+            margin-right: .05rem;
+            height: .2rem;
+            width: .2rem;
+            
+        }
+        &::before{
+            content: "";
+            display: block;
+            position: absolute;
+            left: 0;
+            top: 10%;
+            height: 80%;
+            background-color: #000;
+            width: 4px;
+            border-radius: 4px;
+        }
+    }  
+}
+.sound-block{
+    padding: .15rem 0;
+    p{
+        font-size: .13rem;
+        color: #979797;
+    }
+    .sound-block-recommend{
+        display:flex;
+        flex-wrap:wrap;
+        a{
+            width:33%;
+            padding:0 .06rem;
+            padding-top:.1rem;
+            span{
+                display:inline-block;
+                width:1.06rem;
+                white-space: nowrap;
+                text-overflow: ellipsis;
+                overflow: hidden;
+                margin-top: 2px;
+                color: #424242;
+                font-size: .13rem;
+            }
+        }
+    }
+    .sound-block-like{
+        display:flex;
+        flex-wrap:wrap;
+        align-items: center;
+        a{
+            width: 33%;
+            height: 1.63rem;
+            padding-top:.1rem;
+        img{
+            width: 1.06rem;
+            height: 1.06rem;
+            display: block;
+            overflow: hidden;
+            position: relative;
+            border-radius: 4px;
+            background-repeat: no-repeat;
+            background-position: 50%;
+            background-size: 100%;
+        }
+        .channel-body-title{
+            width: 100%;
+            height: .26rem;
+            word-break: break-all;
+            text-overflow: ellipsis;
+            overflow: hidden;
+            box-orient: vertical;
+            line-clamp: 2;
+            line-height: .18rem;
+            margin: 4px 0;
+            color: #616161;
+            font-size: .13rem;
+        }
+        .channel-body-bottom{
+            display: flex;
+            justify-content: space-between;
+            span{
+                position: relative;
+                padding-left: .14rem;
+                font-size: .11rem;
+                line-height: .11rem;
+                color: #bdbdbd;
+            }
+            .channel-body-palytimes{
+                &::before{
+                    background-size: 56px 55px;
+                    background-position: -44px -14px;
+                    background-image: url('//static.missevan.com/assets/m/images/build/sprite-icons-thumbnails@2x.f1c29abf.png');
+                    content: "";
+                    display: block;
+                    position: absolute;
+                    left: 0;
+                    height: .1rem;
+                    width: .12rem;
+                }
+            }
+            .channel-body-comments{
+                &::before{
+                    background-size: 56px 55px;
+                    background-position: -44px -14px;
+                    background-image: url('//static.missevan.com/assets/m/images/build/sprite-icons-thumbnails@2x.f1c29abf.png');
+                    content: "";
+                    display: block;
+                    position: absolute;
+                    left: 0;
+                    height: .1rem;
+                    width: .12rem;
+                }
+            }
+        }
+    }
+    }
+}
+}
+`
+const SoundInfo = Styled.div`
+                display:flex;
+                flex-direction:column;
+                padding-bottom:.18rem;
+                >p:nth-of-type(1){
+                    font-size: .14rem;
+                    font-weight: 400;
+                    min-height: .24rem;
+                    line-height: .24rem;
+                }
+                ul{
+                    display:flex;
+                    list-style-type: none;
+                    height: .14rem;
+                    line-height: .14rem;
+                    font-size: .1rem;
+                    color: #757575;
+                    align-items:center;
+                    padding : .1rem 0;
+                    li{
+                        padding-left: .16rem;
+                        margin-right: .2rem;
+                        position: relative;
+                        &::before{
+                            content: "";
+                            position: absolute;
+                            top: 1px;
+                            left: 1px;
+                            width: .12rem;
+                            height: .1rem;
+                            background-repeat: no-repeat;
+                            background-size: 100%;
+                            background-size: 56px 55px;
+                            background-image: url('//static.missevan.com/assets/m/images/build/sprite-icons-thumbnails@2x.f1c29abf.png');
+                        }
+                    }
+                    li:nth-of-type(1){
+                        &::before{
+                        background-position: -44px -14px;
+                        }
+                    }
+                    li:nth-of-type(2){
+                        &::before{
+                            background-position: -44px -26px;
+                        }
+                        
+                    }
+                        
+                    }
+                    .infoBox{
+                        .intro{
+                            font-size: .12rem;
+                            color: #9e9e9e;
+                            width: 100%;
+                            word-break: break-all;
+                            line-height:.2rem;
+                            height:${props => props.value ==='更多' ?'.4rem':'auto'}
+                            
+                            overflow:hidden;
+                        }
+                    }
+                    span{
+                        margin-left: calc(100% - 50px);
+                        color: #c14a3f;
+                        font-size: .12rem;
+                    }
 
 `
-
 
 const SoundActionContainer = Styled.div`
         display:flex;
@@ -242,10 +440,109 @@ const SoundActionContainer = Styled.div`
             border-radius:.05rem;
         }
 `
+const SoundDrama = Styled.a`
+    height: 1.04rem;
+    width:100%;
+    display:flex;
+    padding: .18rem 2px .18rem 0;
+    border-bottom: 1px solid #e0e0e0;
+   
+    .soundDramaRight{
+        display:flex;
+        justify-content:space-between;
+        align-items:center;
+        .soundDramaRightText{
+            padding-left:.1rem;
+            display:flex;
+            flex-direction:column;
+            justify-content:space-around
+            span{
+                overflow: hidden;
+                text-overflow: ellipsis;
+                white-space: nowrap;
+                color: #9e9e9e;
+                font-size: .12rem;
+            }
+            p{
+                overflow: hidden;
+                text-overflow: ellipsis;
+                white-space: nowrap;
+                color: #000;
+                font-size: .14rem;
+            }
+            display:flex;
+            flex-direction:column;
+           
+        }
+        .soundDramaRightBtn{
+            display:flex;
+            justify-content:center
+            align-items:center;
+            margin-left:.65rem;
+            height:.26rem;
+            width:.65rem;
+            background-color: #c14a3f;
+            color: #fff;
+            border-radius: 3px;
+        }
+    }
+
+`
+
+const SoundCV = Styled.div`
+.cv-in-sound-list{
+    display:flex;
+    .cv-in-sound{
+        display:flex;
+        flex-direction:column;
+        justify-content:center;
+        padding:.05rem;
+        padding-left:.15rem;
+        div{
+            display:flex;
+            justify-content:center;
+        }
+        p{
+            height: .18rem;
+            margin-top: 5px;
+            font-size: .12rem;
+            line-height: .18rem;
+        }
+        span{
+            height: .16rem;
+            color: #9e9e9e;
+            font-size: 10px;
+            line-height: .16rem;
+            overflow: hidden;
+            text-align: center;
+            -o-text-overflow: ellipsis;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+        }
+}
+}
+
+
+`
 
 const BorderedSoundActionContainer = border({
     component:SoundActionContainer,
     width:'0 0 1px 0'
 })
 
-export  { SoundDetailStyled,BorderedSoundActionContainer }
+const BorderedSoundInfo = border({
+    component:SoundInfo,
+    width:'0 0 1px 0'
+})
+
+const BorderedSoundDrama = border({
+    component:SoundDrama,
+    width:'0 0 1px 0'
+})
+
+const BorderedSoundCV = border({
+    component:SoundCV,
+    width:'0 0 1px 0'
+})
+
+export  { SoundDetailStyled,BorderedSoundActionContainer,BorderedSoundInfo,BorderedSoundDrama,BorderedSoundCV }
